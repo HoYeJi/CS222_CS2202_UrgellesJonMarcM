b820e66ec5e90a6a1a991b84a34cc15d172b1545
Jon Marc Urgelles <jmmurgelles@gmail.com>
2026-02-09 10:21:08 +0800

    Added InterestCalculator

---
+++ Practice Activities/CurrencyConverter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class CurrencyConverter
+{
+    static void Main()
+    {
+        Console.Write("Enter amount in USD: ");
+        double amountUSD = double.Parse(Console.ReadLine()!);
+
+        Console.Write("Enter exchange rate from USD to EUR: ");
+        double exchangeRate = double.Parse(Console.ReadLine()!);
+
+        double amountEUR = amountUSD * exchangeRate;
+
+        Console.WriteLine("Amount in EUR: " + amountEUR.ToString("F2"));
+    }
+}
