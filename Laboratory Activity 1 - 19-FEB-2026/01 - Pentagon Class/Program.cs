ff5fd170a60556bd9dc7c824e8dab72263deb290
Ji <jmmurgelles@gmail.com>
2026-02-19 08:47:34 +0800

    Add files via upload

---
+++ Laboratory Activity 1 - 19-FEB-2026/01 - Pentagon Class/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace PentagonClass
+{
+    class Pentagon
+    {
+        public double side;
+
+        public Pentagon()
+        {
+            side = 0;
+        }
+
+        public Pentagon(double sideLength)
+        {
+            side = sideLength;
+        }
+
+        public double GetPerimeter()
+        {
+            return 5 * side;
+        }
+
+        public double GetArea()
+        {
+            double formula = Math.Sqrt(5 * (5 + 2 * Math.Sqrt(5)));
+            return (formula * Math.Pow(side, 2)) / 4;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Side length: " + side);
+            Console.WriteLine("Perimeter: " + GetPerimeter().ToString("F3"));
+            Console.WriteLine("Area: " + GetArea().ToString("F3"));
+            Console.WriteLine();
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter side for p1: ");
+            double side1 = double.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("\nPentagon p1");
+            Console.WriteLine("---------------------------");
+            Pentagon p1 = new Pentagon(side1);
+            p1.Display();
+
+            Console.Write("Enter side for p2: ");
+            double side2 = double.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("\nPentagon p2");
+            Console.WriteLine("---------------------------");
+            Pentagon p2 = new Pentagon(side2);
+            p2.Display();
+        }
+    }
+}
---
+++ Laboratory Activity 1 - 19-FEB-2026/02 - Music Player Organizer/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace MusicPlaylistOrganizer
+{
+    class Song
+    {
+        public string title;
+        public string artist;
+        public double duration;
+
+        public Song()
+        {
+            title = "Unknown";
+            artist = "Unknown";
+            duration = 0.0;
+        }
+
+        public Song(string songTitle, string songArtist)
+        {
+            title = songTitle;
+            artist = songArtist;
+            duration = 0.0;
+        }
+
+        public Song(string songTitle, string songArtist, double songDuration)
+        {
+            title = songTitle;
+            artist = songArtist;
+            duration = songDuration;
+        }
+
+        public void DisplaySong()
+        {
+            Console.WriteLine("{0,-15} {1,-15} {2,5:F2}", title, artist, duration);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Songs to add: ");
+            int size = int.Parse(Console.ReadLine()!);
+
+            Song[] myPlaylist = new Song[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("\nSong #" + (i + 1));
+
+                Console.Write("Title: ");
+                string t = Console.ReadLine();
+
+                Console.Write("Artist: ");
+                string a = Console.ReadLine();
+
+                Console.Write("Duration (minutes): ");
+                double d = double.Parse(Console.ReadLine());
+
+                myPlaylist[i] = new Song(t, a, d);
+            }
+
+            Console.WriteLine("\n=== || MY PLAYLIST || ===");
+            Console.WriteLine("{0,-15} {1,-15} {2,5}", "Title", "Artist", "Time");
+            Console.WriteLine("--------------------------------------------");
+
+            double totalTime = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                myPlaylist[i].DisplaySong();
+                totalTime = totalTime + myPlaylist[i].duration;
+            }
+
+            double average = totalTime / size;
+
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Total Duration: " + totalTime.ToString("F2") + " mins");
+            Console.WriteLine("Average Duration: " + average.ToString("F2") + " mins");
+        }
+    }
+}
